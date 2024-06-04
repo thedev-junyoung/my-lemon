8fa1a70e9b04c20081c170354704a54831936230
thedev-junyoung <thedev.junyoung@gmail.com>
2024-06-04 18:28:30 +0900

    Chatfooter 수정

--- frontend/src/components/ChatInput/ChatInput.tsx
+++ frontend/src/components/ChatInput/ChatInput.tsx
@@ -5,24 +5,25 @@ import Button from '../Button/Button';
 
 const ChatInput: React.FC<ChatInputProps> = ({ input, setInput, sendMessage }) => {
   return (
-    <footer id="chat-footer">
-      <input
-        type="text"
-        id="chat-input"
-        placeholder="Message To LEMON..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            sendMessage();
-          }
-        }}
-      />
-      <Button label={'Send'} onClick={function (): void {
-        alert('버튼 이벤트 추가하기')
-        // throw new Error('Function not implemented.');
-      } }/>
-    </footer>
+    <div id="chat-footer">
+      <div id="chat-footer-inputarea">
+        <textarea
+          id="chat-input"
+          placeholder="Message To LEMON..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              sendMessage();
+            }
+          }}
+        />
+        <Button label={'Send'} onClick={function (): void {
+          alert('버튼 이벤트 추가하기')
+          // throw new Error('Function not implemented.');
+        }} />
+      </div>
+    </div>
   );
 };
 
