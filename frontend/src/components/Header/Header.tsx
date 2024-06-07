9a7dc8eb3761062df673b0f4f2925759c4e7981a
thedev-junyoung <thedev.junyoung@gmail.com>
2024-06-07 11:33:18 +0900

    로고 사이즈 및 경로 변경

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
       <div className="logo mb-5">
-        <img src="/logo.png" alt="LEMON Logo" className="w-100 h-200" />
+        <img src="/logo/logo150x75.png" alt="LEMON Logo" className="w-100 h-200" />
       </div>
   );
 };
