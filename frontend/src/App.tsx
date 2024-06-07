a6a9f23cac459e008dab524dea4207840f2fdfb8
thedev-junyoung <thedev.junyoung@gmail.com>
2024-06-07 13:08:56 +0900

    css -> 테일윈드 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
@@ -8,11 +7,13 @@ import Login from './pages/Login/Login';
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Login />} />
-        {/* 다른 페이지 경로들도 여기에 추가할 수 있습니다 */}
-      </Routes>
+      <div className="flex flex-col h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          {/* 다른 페이지 경로들도 여기에 추가할 수 있습니다 */}
+        </Routes>
+      </div>
     </Router>
   );
 };
