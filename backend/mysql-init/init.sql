f71d9d66bdf2e8afe3222beec848a5311a90bb81
thedev-junyoung <thedev.junyoung@gmail.com>
2024-06-10 15:44:37 +0900

    설정 변경

--- backend/mysql-init/init.sql
+++ backend/mysql-init/init.sql
@@ -5,10 +5,14 @@ CREATE DATABASE IF NOT EXISTS lemon CHARACTER SET utf8mb4 COLLATE utf8mb4_unicod
 CREATE USER IF NOT EXISTS 'lemon'@'%' IDENTIFIED BY '081908';
 
 -- 'lemon' 사용자의 인증 플러그인을 'caching_sha2_password'로 설정
-ALTER USER 'lemon'@'%' IDENTIFIED WITH mysql_native_password BY '081908';
+ALTER USER 'lemon'@'%' IDENTIFIED WITH caching_sha2_password BY '081908';
+
+-- 'root' 사용자의 인증 플러그인을 'caching_sha2_password'로 설정
+ALTER USER 'root'@'%' IDENTIFIED WITH caching_sha2_password BY '081908';
 
 -- 'lemon' 사용자에게 모든 데이터베이스에 대한 모든 권한 부여
 GRANT ALL PRIVILEGES ON *.* TO 'lemon'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
 -- 변경 사항 적용
 FLUSH PRIVILEGES;
